9ff50b1365770154ba17c65473ca20c7ec323f1f
Cooper Pellaton <c@cepp.at>
2015-10-30 01:25:35 -0400

    Updated the max int of the rand int to be 59.

--- cracker.py
+++ cracker.py
@@ -19,11 +19,11 @@ def randomletter(numberofletters):
     # Takes a int and will retruns a list
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
                'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-               'w', 'x', 'y', 'z', ' ', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[',']', '\', '|', ':', ';', '"', ''', '<', '>', ',',  '.', '?', '/']
+               'w', 'x', 'y', 'z', ' ', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[',']', '\', '|', ':', ';', '"', ''', '<', '>', ',', '.', '?', '/']
     listofletters = []
     index = numberofletters
     while index != 0:
-        listofletters.append(letters[random.randint(0, 26)])
+        listofletters.append(letters[random.randint(0, 59)])
         index = index - 1
     return listofletters
 
